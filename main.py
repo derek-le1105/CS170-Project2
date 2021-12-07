5064e68621fd3fdbe56c0dc8d5d1bd12dea290ef
Derek Le <64372005+derek-le1105@users.noreply.github.com>
2021-12-07 02:26:28 -0800

    Implemented Backward Elimination

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ import time
 import copy
 import numpy as np
 start_time = time.time()
+backwardSelected = False
 
 def main():     #csv file contains my name and small(56) and large(28) data set i should use        SMALL 86: 57 seconds to run, LARGE 27: 
     print("Welcome to the Feature Selection Algorithm")
-    #fileName = "Data/" + input("Type in the name of the file you want to test: ")
-    fileName = "Data/Ver_2_CS170_Fall_2021_LARGE_data__27.txt"
-    #algDec = input("Type in the corresponding algorithm you want to run \n1) Forward Selection \n2) Backward Elimination\n")
-    algDec = 1
+    datasetSize = input("Do you want to test the 'LARGE' or 'SMALL' dataset? Input large/small in CAPS please: ").upper()
+    datasetNumber = input("Which dataset do you want to test? Please input from 1-100 for the desired dataset: ")
+    fileName = "Data/Ver_2_CS170_Fall_2021_" + datasetSize + "_data__" + datasetNumber + ".txt"
+    algDec = int(input("Do you want to run Forward Selection or Backward Elimination? Enter 1 or 2 for the respective algorithm: "))
     print(f"Running {fileName}")
     data = textfileToNPMatrix(fileName)
     if algDec == 1:
         a, b = feature_search_demo(data)
+    elif algDec == 2:
+        global backwardSelected 
+        backwardSelected = True
+        backwardElimination(data)
     print(f"Highest accuracy recorded was at: {a} with a set of: {b}")
     print(f"Program took {round(time.time() - start_time, 3)} seconds to run")
 
@@ -22,10 +27,34 @@ def textfileToNPMatrix(file):
     matrix = np.loadtxt(file)
     return matrix
 
+def backwardElimination(data):
+    numOfInstances, numOfFeatures = data.shape
+    print(f"This dataset has {numOfFeatures - 1} features(excluding class attribute) with {numOfInstances} instances")
+    current_set_of_features = [x for x in range(1, numOfFeatures)]
+    timeElapsed = time.time() - start_time
+    for i in range(1, numOfFeatures):
+        print(f"\nOn the {i}th level of the search tree")
+        feature_to_remove_at_this_level = []
+        least_so_far_accuracy = 0
+
+        for j in range(1, numOfFeatures):
+            if j in current_set_of_features:
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features, j)
+                print(f"--Removing feature {j} from current set yields accuracy of {accuracy} ")
+                if accuracy > least_so_far_accuracy:
+                    least_so_far_accuracy = accuracy
+                    feature_to_remove_at_this_level = j
+        
+        current_set_of_features.remove(feature_to_remove_at_this_level)
+        print(f"At level {i}, I removed feature {feature_to_remove_at_this_level} from the current set to achieve an accuracy of {least_so_far_accuracy}")
+        if(timeElapsed > 7200):
+            break
+    return
+
 def feature_search_demo(data):
     numOfInstances, numOfFeatures = data.shape  #numOfInstances is number of rows in np array, numOfFeatures is number of columns
     print(f"This dataset has {numOfFeatures - 1} features(excluding class attribute) with {numOfInstances} instances")
-    highestAccuracy = 0
+    highestAccuracyFound = 0
     indexAtHighest = 0
     current_set_of_features = []
     accDecreasing = False
@@ -39,7 +68,7 @@ def feature_search_demo(data):
         for j in range(1, numOfFeatures):
             if j not in current_set_of_features:
                 accuracy = leave_one_out_cross_validation(data, current_set_of_features, j)
-                print(f"--Considering adding the {j} feature with accuracy of {accuracy}")
+                print(f"--Considering adding feature {j} with accuracy of {accuracy}")
                 if accuracy > best_so_far_accuracy:
                     best_so_far_accuracy = accuracy
                     feature_to_add_at_this_level = j
@@ -49,19 +78,15 @@ def feature_search_demo(data):
         
         current_set_of_features.append(feature_to_add_at_this_level)
         print(f"At level {i}, I added feature {feature_to_add_at_this_level} to the current set with accuracy of {best_so_far_accuracy}")
-        if (timeElapsed > 3200) or (accDecreasing and highestAccuracy > best_so_far_accuracy):
-            break
-        if(highestAccuracy > best_so_far_accuracy):
+        if (timeElapsed > 7200) or (accDecreasing and highestAccuracyFound > best_so_far_accuracy):      #called before the next if statement so if algorithm notices accuracy decreasing, we run one more time 
+            break                                                                                   #to see if there is a local maxima 
+        if(highestAccuracyFound > best_so_far_accuracy):                                                 #when we first notice the accuracy decreasing, give a warning to user 
             accDecreasing = True
             print("Warning! Accuracy is beginning to decrease so we'll continue search in case of local maxima!")
-    return highestAccuracy, current_set_of_features[:indexAtHighest]
+    return highestAccuracyFound, current_set_of_features[:indexAtHighest]
 
 def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add):      #current set of features is a list of the features we want to test
-    newData = copy.deepcopy(data)
-    for i in range(newData.shape[0]):       #makes unwanted columns equal to 0
-        for j in range(1, newData.shape[1]):
-            if j not in current_set_of_features and j != feature_to_add:
-                newData[i][j] = 0
+    newData = setColsToZero(data, current_set_of_features, feature_to_add)
     number_correctly_classified = 0
     for i in range(newData.shape[0]):           #goes through each instance(row) in data
         object_to_classify = newData[i][1:]
@@ -72,18 +97,29 @@ def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add
 
         for j in range(newData.shape[0]):        #iterate over every other instance besides ith instance
             if j != i:
-                #print(f"Ask if {i} is nearest neighbor with {j}")
                 distance = np.sqrt(sum(np.square(object_to_classify - newData[j][1:])))
                 if distance < nearest_neighbor_distance:                                            
                     nearest_neighbor_distance = distance
                     nearest_neighbor_location = j
                     nearest_neighbor_label = newData[nearest_neighbor_location][0]
-
-        #print(f"Object {i} is class {label_object_to_classify}")
-        #print(f"Its nearest neighbor is {nearest_neighbor_location}, which is in class {nearest_neighbor_label}")
         if label_object_to_classify == nearest_neighbor_label:
             number_correctly_classified +=1
     accuracy = round((number_correctly_classified / data.shape[0]) * 100, 3)
-    del newData
     return accuracy
+
+def setColsToZero(data, current_set_of_features, feature_to_add):
+    newData = copy.deepcopy(data)           #make a copy of the dataset that we can manipulate without changing the original dataset
+    if backwardSelected:
+        for i in range(newData.shape[0]):       #makes unwanted columns equal to 0
+            for j in range(1, newData.shape[1]):
+                if (j in current_set_of_features and j == feature_to_add) or j not in current_set_of_features:
+                    newData[i][j] = 0
+    else:
+        for i in range(newData.shape[0]):       #makes unwanted columns equal to 0
+                for j in range(1, newData.shape[1]):
+                    if j not in current_set_of_features and j != feature_to_add:
+                        newData[i][j] = 0
+    return newData
+
 main()
+
